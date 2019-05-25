26eca9bf43fa26634cf4c5a8808217c94be6b6dc
royinx <royfork@hotmail.com>
2019-05-25 16:13:04 +0800

    installation bash

---
+++ ubuntu/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#new installation and python
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential
+sudo apt-get install -y make 
+sudo apt-get dist-upgrade -y
+sudo apt-get install -y python3-pip
+sudo apt-get install -y libncursesw5-dev libgdbm-dev libc6-dev
+sudo apt-get install -y zlib1g-dev libsqlite3-dev tk-dev
+sudo apt-get install -y libssl-dev openssl
+sudo apt-get install -y libffi-dev
+
+
