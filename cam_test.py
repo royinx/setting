d484ad132847feb316a065e7b669cadf6a610200
royinx <royfork@hotmail.com>
2019-06-24 16:58:27 +0800

    update cam_test

--- cam_test.py
+++ cam_test.py
@@ -1,12 +1,11 @@
 import cv2
-cap = cv2.VideoCapture(-1)
+import sys
+cap = cv2.VideoCapture(int(sys.argv[1]))
 
 while True:
     ret, frame = cap.read()
-    cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
-
