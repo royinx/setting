4903a8c19d825f7184c71cc587e118761be9390e
royinx <royfork@hotmail.com>
2019-06-29 08:49:48 +0800

    cam_test add url

--- cam_test.py
+++ cam_test.py
@@ -1,6 +1,10 @@
 import cv2
 import sys
-cap = cv2.VideoCapture(int(sys.argv[1]))
+try:
+	video_source = int(sys.argv[1])
+except:
+	video_source = str(sys.argv[1])
+cap = cv2.VideoCapture(video_source
 
 while True:
     ret, frame = cap.read()
