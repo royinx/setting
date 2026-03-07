8a1c08b9d82b1016a08a4b3cfdf7099394fc81a8
royinx <royfork@hotmail.com>
2026-03-06 23:13:46 -0500

    feat: add arch

---
+++ arch/update.sh
@@ -0,0 +1,14 @@
+sudo pacman -Sy --noconfirm git base-devel docker-compose
+
+# 安裝 yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
+# 4090 nvidia driver
+sudo pacman -Sy nvidia-open
+# yay -S nvidia-580xx-utils --noconfirm
+
+yay -S nvidia-container-toolkit
+sudo systemctl restart docker
+docker run --rm --gpus all python:3.11-slim nvidia-smi
